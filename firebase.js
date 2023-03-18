52f1f427b7218badf1a47057a463fc1f8880d6bd
krishthukral <59071300+krishthukral@users.noreply.github.com>
2023-03-18 12:34:50 -0700

    x

--- firebase.js
+++ firebase.js
@@ -5,15 +5,19 @@ import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import { getAuth } from "firebase/auth";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCTp-Gx5UZSBJKcvkHeIxc2K5N9Aq3ocYg",
-  authDomain: "habittracker-59231.firebaseapp.com",
-  projectId: "habittracker-59231",
-  storageBucket: "habittracker-59231.appspot.com",
-  messagingSenderId: "404079753933",
-  appId: "1:404079753933:web:ce8341748e1949ebfbc784",
+  apiKey: "AIzaSyDzo2eOEqPUCbZW9wXC0UxoaiKTldE6kGo",
+  authDomain: "habitminder-dba4c.firebaseapp.com",
+  projectId: "habitminder-dba4c",
+  storageBucket: "habitminder-dba4c.appspot.com",
+  messagingSenderId: "269249719680",
+  appId: "1:269249719680:web:14c22e94331aa963bc2565"
 };
 
 // Initialize Firebase
--- screens/Overview.js
+++ screens/Overview.js
@@ -8,18 +8,29 @@ function Overview() {
   return (
     <ScrollView>
       <View className="flex-1 items-center bg-black">
+
         <View
           className="flex flex-col max-w-[85%] mt-4 p-4 rounded-lg"
           style={{ backgroundColor: "#333333" }}
         >
           {/* Step 1: Add Daily Goals title and description */}
-          {/* ... */}
+          <View className="flex flex-row justify-between">
+            <CircleProgressMeter
+              fill={75}
+              tintColor = "#e740f40"
+              backgroundColor = "#3d5875"
+              iconName = "run"
+              title = "Run"
+            />
+            </View>
 
           {/* Step 2: Add CircleProgressMeter components for each habit */}
           {/* ... */}
 
           {/* Step 3: Add progress bar and percentage */}
           {/* ... */}
+          <Progress.Bar progress={0.5} width={200} />
+            
         </View>
         <View className="w-full px-8 pt-4">
           {/* Step 4: Add Categories title */}
@@ -28,9 +39,16 @@ function Overview() {
         <View className="flex flex-row flex-wrap items-center justify-center">
           {/* Step 5: Add HabitCard components for each category */}
           {/* ... */}
+          <HabitCard
+            title="Run"
+            description="Run 3 miles"
+            backgroundColor="#3d5875"
+            iconName="run"
+          />
         </View>
       </View>
     </ScrollView>
+
   );
 }
 
